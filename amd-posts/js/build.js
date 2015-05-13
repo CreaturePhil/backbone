e04bf5a88778a6d866bca98e7ea0547aa9a924c7
Phil <birkal@outlook.com>
2015-05-12 21:04:46 -0700

    Add build

---
+++ amd-posts/js/build.js
@@ -0,0 +1,11 @@
+({
+  baseUrl: ".",
+  name: "main",
+  out: "main-built.js",
+  paths: {
+    jquery: 'lib/jquery.min',
+    underscore: 'lib/underscore-min',
+    backbone: 'lib/backbone-min',
+    text: 'lib/text'
+  }
+})
