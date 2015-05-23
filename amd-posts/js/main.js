18fb79a4a71c644e2ffe566ac7240cef920e739b
Phil <birkal@outlook.com>
2015-05-23 13:26:09 -0700

    Add shim config

--- amd-posts/js/main.js
+++ amd-posts/js/main.js
@@ -1,9 +1,22 @@
 require.config({
   paths: {
-    jquery: 'lib/jquery.min',
-    underscore: 'lib/underscore-min',
-    backbone: 'lib/backbone-min',
-    text: 'lib/text'
+    jquery: 'vendor/jquery.min',
+    underscore: 'vendor/underscore-min',
+    backbone: 'vendor/backbone-min',
+    text: 'vendor/text',
+    foundation: 'vendor/foundation.min'
+  },
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'foundation': {
+      deps: ['jquery']
+    }
   }
 });
 
@@ -11,8 +24,9 @@ require([
   'jquery',
   'backbone',
   'views/posts',
-  'views/edit'
-], function($, Backbone, PostsView, EditView) {
+  'views/edit',
+  'foundation'
+], function($, Backbone, PostsView, EditView, Foundation) {
   $.ajaxPrefilter(function(options) {
     options.url = 'http://localhost:3000' + options.url;
   });
@@ -46,17 +60,21 @@ require([
       if (!this.currentView) return;
       this.currentView.remove();
     },
+
+    render: function() {
+      $content.html(this.currentView.render().el); 
+    },
     
     home: function() {
       this.before();
       this.currentView = new PostsView();
-      $content.html(this.currentView.render().el);
+      this.render();
     },
 
     edit: function() {
       this.before();
       this.currentView = new EditView(this);
-      $content.html(this.currentView.render().el);
+      this.render();
     }
   });
 
